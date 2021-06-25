92a76ff81eaf84ea40f1079d1b2ae819291b78de
Theo Petit <theo.p83@gmail.com>
2021-06-25 13:34:18 +0200

    add table.c

---
+++ table.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <gmp.h>
+
+#include <math.h>
+
+#include "rns.h"
+
+#include "tests.c"
+
+#include "rnsv.h"
+
+int main(void)
+{
+
+    return 0;
+}

3b831edecd637af4bba034c0297a27f8392be80a
Theo Petit <theo.p83@gmail.com>
2021-06-25 11:42:15 +0200

    fixing 0 in table

--- tests.c
+++ tests.c
@@ -7,8 +7,8 @@
 #include <sys/syscall.h>
 
 #define NFUNS 5		 // nombre de fois qu'on appelle la fonction à tester entre 2 mesures
-#define NTEST 500	 // nombre de fois ou on repete le meme jeu de donnees
-#define NSAMPLES 100 // nombre differents de jeu de donnees
+#define NTEST 50	 // nombre de fois ou on repete le meme jeu de donnees
+#define NSAMPLES 10 // nombre differents de jeu de donnees
 
 /**** Measurements procedures according to INTEL white paper
 

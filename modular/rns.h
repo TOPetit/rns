5dcd0efe32b244cbb961725618944a85d7b4d0dc
Theo Petit <theo.p83@gmail.com>
2021-06-28 11:08:21 +0200

    argument error fix

--- modular/rns.h
+++ modular/rns.h
@@ -3,7 +3,7 @@
 // RNS arithmetic functions
 void add_rns(int64_t *rop, struct rns_base_t *base, int64_t *pa, int64_t *pb);
 void sub_rns(int64_t *rop, struct rns_base_t *base, int64_t *pa, int64_t *pb);
-void mul_rns(int64_t *rop, struct rns_base_t *base, int64_t *pa, int64_t *pb);
+void mul_rns(int64_t *rop, struct rns_base_t *base, int64_t *pa, int64_t *pb, int64_t n);
 void mult_mod_rns(int64_t *rop, int64_t *pa, int64_t *pab, int64_t *pb, int64_t *pbb, struct mod_mul_t *mult, int64_t *tmp[3]);
 
 void init_rns(struct rns_base_t *base);

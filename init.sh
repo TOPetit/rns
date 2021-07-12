8849be37370ecd3e758814ffb33ff66f80e20522
Theo Petit <theo.p83@gmail.com>
2021-07-12 09:33:05 +0200

    adding deactivating turbo boost in init

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ modprobe msr
 echo 2 | dd of=/sys/devices/cpu/rdpmc
 echo 2 | dd of=/sys/bus/event_source/devices/cpu/rdpmc
 wrmsr -a 0x38d 0x0333
+/bin/sh -c "/usr/bin/echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
